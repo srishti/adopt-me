1db98caa81becbb5840ba2941646c78d03c6eca0
Srishti Gupta <srishtigupta@Srishtis-MacBook-Pro.local>
2021-03-04 18:00:46 +0530

    converted project to jsx and added react related eslint config

--- src/App.js
+++ src/App.js
@@ -3,26 +3,14 @@ import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Sheru",
-      animal: "Dog",
-      breed: "INDog",
-    }),
-    React.createElement(Pet, {
-      name: "Maxi",
-      animal: "Dog",
-      breed: "German Shepherd",
-    }),
-    React.createElement(Pet, {
-      name: "Ruby",
-      animal: "Dog",
-      breed: "Pomeranian",
-    })
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Sheru" animal="Dog" breed="INDog" />
+      <Pet name="Maxi" animal="Dog" breed="German Shepherd" />
+      <Pet name="Ruby" animal="Dog" breed="Pomeranian" />
+    </div>
   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
