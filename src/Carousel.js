c40caf36ee679feb00bf92b0194a9a52de0f0b47
Srishti Gupta <srishtigupta@Srishtis-MacBook-Pro.local>
2021-03-09 18:07:40 +0530

    modified Details component, added custom Babel config, added Carousel component

---
+++ src/Carousel.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+class Carousel extends React.Component {
+  // public class property
+  state = {
+    photos: [],
+    active: 0,
+  };
+
+  static getDerivedStateFromProps({ media }) {
+    let photos = ["http://placecorgi.com/600/600"];
+
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+
+    return { photos };
+  }
+
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  };
+
+  render() {
+    const { photos, active } = this.state;
+
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              key={photo}
+              src={photo}
+              alt="animal thumbnail"
+              onClick={this.handleIndexClick}
+              data-index={index}
+              className={index === active ? "active" : ""}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
